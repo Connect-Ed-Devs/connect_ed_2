da43651bda02288be888606ad2360a4d2e05b23d
arrana16 <69128309+arrana16@users.noreply.github.com>
2025-03-10 15:54:37 +0300

    feat: added initial styles

---
+++ lib/frontend/styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+ColorScheme lightScheme = const ColorScheme(
+  brightness: Brightness.light,
+  primary: Color.fromARGB(255, 0, 66, 112),
+  onPrimary: Colors.white,
+  secondary: Color.fromARGB(255, 160, 207, 235),
+  onSecondary: Colors.black,
+  error: Color.fromARGB(255, 241, 114, 114),
+  onError: Colors.black,
+  surface: Colors.white,
+  onSurface: Colors.black,
+);
+
+ColorScheme darkScheme = const ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color.fromARGB(255, 160, 207, 235),
+  onPrimary: Colors.black,
+  secondary: Color.fromARGB(255, 0, 66, 112),
+  onSecondary: Colors.white,
+  error: Color.fromARGB(255, 160, 31, 31),
+  onError: Colors.white,
+  surface: Color.fromARGB(255, 16, 16, 16),
+  onSurface: Colors.white,
+);
