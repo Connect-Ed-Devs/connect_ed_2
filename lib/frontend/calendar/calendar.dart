fbe2747553dcda57537015f580b67a6362bdee8c
arrana16 <69128309+arrana16@users.noreply.github.com>
2025-03-11 19:18:59 +0300

    feat: created app bar

--- lib/frontend/calendar/calendar.dart
+++ lib/frontend/calendar/calendar.dart
@@ -1,3 +1,4 @@
+import 'package:connect_ed_2/frontend/setup/app_bar.dart';
 import 'package:flutter/material.dart';
 
 class CalendarPage extends StatefulWidget {
@@ -10,6 +11,13 @@ class CalendarPage extends StatefulWidget {
 class _CalendarPageState extends State<CalendarPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          CEAppBar(title: "Calendar", showBackButton: false),
+          SliverToBoxAdapter(child: Container(height: 700, color: Colors.grey)),
+        ],
+      ),
+    );
   }
 }
